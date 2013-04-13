babe87c5ba3d2a38ef212d4bd677255633ae7cd1
Oriol Blanc <oriolblanc@gmail.com>
2013-04-13 14:19:09 +0200

    Player

--- app/models/Player.java
+++ app/models/Player.java
@@ -1,10 +1,58 @@
 package models;
 
-public class Player {
-	public int score;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import play.data.validation.Required;
+import play.data.validation.Unique;
+import play.db.jpa.GenericModel;
+
+@Entity
+public class Player extends GenericModel{
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+    
+    @Required
+    @Unique
+    private String username;
+
+    @Required
+    @Unique
+    private String email;
+    
+    private String avatar;
+    
+	private int countGames;
+
+	private int countWins;
+
+	private int countLosts;
+
+	private int countDraws;
+
+	public int score; // TODO: Should be private
 	
 	public Player(int score) {
 		super();
 		this.score = score;
 	}
+	
+	public Player(String username, String email) {
+		
+		super();
+		this.username = username;
+		this.email = email;
+	}
+	
+	/*
+	 * Getters
+	 */
+
+	
 }
