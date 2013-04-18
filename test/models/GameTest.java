3c7413b47e4350720df098e9a56f222bad42cc94
Oriol Blanc <oriolblanc@gmail.com>
2013-04-18 20:15:44 +0200

    Testing Game Model

--- test/models/GameTest.java
+++ test/models/GameTest.java
@@ -1,13 +1,18 @@
 package models;
+import models.Game.GameInvalidModelException;
+
 import org.junit.Test;
 
 import play.test.UnitTest;
 
 public class GameTest extends UnitTest {
 
-    @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
+    @Test(expected=Game.GameInvalidModelException.class)
+    public void gameAgaingSamePlayer() throws GameInvalidModelException {
+        Player player = new Player("player", "player@mail.com");
+        player.save();
+        
+        new Game(player, player, 0, 0);
     }
 
 }
