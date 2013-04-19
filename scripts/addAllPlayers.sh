7cf642647e56fbcff3e6de2fecf307fb101c71cd
Oriol Blanc <oriolblanc@gmail.com>
2013-04-19 12:09:23 +0200

    Added scripts to insert current data

---
+++ scripts/addAllPlayers.sh
@@ -0,0 +1,29 @@
+echo "Adding Oriol"
+sh addPlayer.sh Oriol oriolblanc@gmail.com
+
+echo "Adding Pepe"
+sh addPlayer.sh Pepe rosanero@gmail.com
+
+echo "Adding Rafa"
+sh addPlayer.sh Rafa rais38@gmail.com
+
+echo "Adding Espada"
+sh addPlayer.sh Espada ijespada@gmail.com
+
+echo "Adding Jordi"
+sh addPlayer.sh Jordi jcollell@gmail.com
+
+echo "Adding Valenti"
+sh addPlayer.sh Valenti vfreixanet@gmail.com
+
+echo "Adding Quique"
+sh addPlayer.sh Quique nriquealvarez@gmail.com
+
+echo "Adding Joan"
+sh addPlayer.sh Joan joanromano@gmail.com
+
+echo "Adding Seb"
+sh addPlayer.sh Seb sebastiaan.vankuyk@gmail.com
+
+echo "Adding Mickey"
+sh addPlayer.sh Mickey mickey.tetrov@gmail.com
