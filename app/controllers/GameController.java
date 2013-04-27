6fc65c18f2badd0f07d61d704f434062b9e122e2
Oriol Blanc <oriolblanc@gmail.com>
2013-04-27 09:30:23 +0200

    Raw requests

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -38,8 +38,8 @@ public class GameController extends Controller {
 		
 		String output = "";
 		for(Game game: games){
-			output = output + "echo \"Adding a game between " + game.getLocal().getUsername() + " and " + game.getVisitor().getUsername() + " with: " + game.getGolsLocal() + "-" + game.getGolsVisitor() + "\n";
-			output = output + "sh addGame.sh " + game.getLocal().getUsername() + " " + game.getVisitor().getUsername() + " " + game.getGolsLocal() + " " + game.getGolsVisitor() + "\n";
+			output = output + "echo \"Adding a game between " + game.getLocal().getUsername() + " and " + game.getVisitor().getUsername() + " with: " + game.getGolsLocal() + "-" + game.getGolsVisitor() + "\"\n";
+			output = output + "sh addGame.sh " + game.getLocal().getUsername() + " " + game.getVisitor().getUsername() + " " + game.getGolsLocal() + " " + game.getGolsVisitor() + "\n\n";
 		}
 		
 		renderText(output);
--- app/controllers/PlayerController.java
+++ app/controllers/PlayerController.java
@@ -2,6 +2,9 @@ package controllers;
 
 import java.util.List;
 
+import controllers.admin.Players;
+
+import models.Game;
 import models.Player;
 import play.data.validation.Required;
 import play.mvc.Controller;
@@ -11,6 +14,19 @@ public class PlayerController extends Controller {
 	public static void players(){
 		List<Player> players = Player.findPlayersSortedByUsername();
 		render(players);
+	}
+
+	public static void playersRaw(){
+		
+		List<Player> players = Player.findAll();
+		
+		String output = "";
+		for(Player player: players){
+			output = output + "echo \"Adding player " + player.getUsername() + " " + player.getEmail() + "\"\n";
+			output = output + "sh addPlayer.sh " + player.getUsername() + " " + player.getEmail() + "\n\n";
+		}
+		
+		renderText(output);
 	}
 	
 	public static void ranking(){
