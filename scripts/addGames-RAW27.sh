45a9d560d9cf2d0095836e537f40d1170df9bb0f
Oriol Blanc <oriolblanc@gmail.com>
2013-04-27 09:39:48 +0200

    Updating scripts

---
+++ scripts/addGames-RAW27.sh
@@ -0,0 +1,356 @@
+echo "Adding a game between Rafa and Pepe with: 1-2"
+sh addGame.sh Rafa Pepe 1 2
+
+echo "Adding a game between Oriol and Espada with: 3-1"
+sh addGame.sh Oriol Espada 3 1
+
+echo "Adding a game between Oriol and Pepe with: 1-5"
+sh addGame.sh Oriol Pepe 1 5
+
+echo "Adding a game between Rafa and Pepe with: 1-2"
+sh addGame.sh Rafa Pepe 1 2
+
+echo "Adding a game between Espada and Rafa with: 0-2"
+sh addGame.sh Espada Rafa 0 2
+
+echo "Adding a game between Oriol and Rafa with: 3-0"
+sh addGame.sh Oriol Rafa 3 0
+
+echo "Adding a game between Jordi and Espada with: 0-4"
+sh addGame.sh Jordi Espada 0 4
+
+echo "Adding a game between Espada and Valenti with: 4-0"
+sh addGame.sh Espada Valenti 4 0
+
+echo "Adding a game between Jordi and Oriol with: 1-2"
+sh addGame.sh Jordi Oriol 1 2
+
+echo "Adding a game between Espada and Rafa with: 1-0"
+sh addGame.sh Espada Rafa 1 0
+
+echo "Adding a game between Rafa and Oriol with: 1-1"
+sh addGame.sh Rafa Oriol 1 1
+
+echo "Adding a game between Rafa and Pepe with: 0-0"
+sh addGame.sh Rafa Pepe 0 0
+
+echo "Adding a game between Rafa and Pepe with: 1-2"
+sh addGame.sh Rafa Pepe 1 2
+
+echo "Adding a game between Oriol and Pepe with: 1-0"
+sh addGame.sh Oriol Pepe 1 0
+
+echo "Adding a game between Rafa and Jordi with: 5-5"
+sh addGame.sh Rafa Jordi 5 5
+
+echo "Adding a game between Oriol and Espada with: 2-0"
+sh addGame.sh Oriol Espada 2 0
+
+echo "Adding a game between Oriol and Jordi with: 2-3"
+sh addGame.sh Oriol Jordi 2 3
+
+echo "Adding a game between Valenti and Rafa with: 1-4"
+sh addGame.sh Valenti Rafa 1 4
+
+echo "Adding a game between Valenti and Oriol with: 0-1"
+sh addGame.sh Valenti Oriol 0 1
+
+echo "Adding a game between Pepe and Quique with: 3-1"
+sh addGame.sh Pepe Quique 3 1
+
+echo "Adding a game between Rafa and Quique with: 2-3"
+sh addGame.sh Rafa Quique 2 3
+
+echo "Adding a game between Pepe and Quique with: 1-0"
+sh addGame.sh Pepe Quique 1 0
+
+echo "Adding a game between Pepe and Quique with: 2-0"
+sh addGame.sh Pepe Quique 2 0
+
+echo "Adding a game between Pepe and Quique with: 3-2"
+sh addGame.sh Pepe Quique 3 2
+
+echo "Adding a game between Pepe and Espada with: 1-0"
+sh addGame.sh Pepe Espada 1 0
+
+echo "Adding a game between Pepe and Quique with: 1-2"
+sh addGame.sh Pepe Quique 1 2
+
+echo "Adding a game between Joan and Oriol with: 0-1"
+sh addGame.sh Joan Oriol 0 1
+
+echo "Adding a game between Joan and Oriol with: 0-0"
+sh addGame.sh Joan Oriol 0 0
+
+echo "Adding a game between Joan and Oriol with: 1-2"
+sh addGame.sh Joan Oriol 1 2
+
+echo "Adding a game between Joan and Oriol with: 5-0"
+sh addGame.sh Joan Oriol 5 0
+
+echo "Adding a game between Joan and Oriol with: 1-0"
+sh addGame.sh Joan Oriol 1 0
+
+echo "Adding a game between Jordi and Espada with: 1-1"
+sh addGame.sh Jordi Espada 1 1
+
+echo "Adding a game between Seb and Oriol with: 0-0"
+sh addGame.sh Seb Oriol 0 0
+
+echo "Adding a game between Espada and Pepe with: 2-3"
+sh addGame.sh Espada Pepe 2 3
+
+echo "Adding a game between Jordi and Pepe with: 1-3"
+sh addGame.sh Jordi Pepe 1 3
+
+echo "Adding a game between Espada and Jordi with: 1-7"
+sh addGame.sh Espada Jordi 1 7
+
+echo "Adding a game between Jordi and Espada with: 1-1"
+sh addGame.sh Jordi Espada 1 1
+
+echo "Adding a game between Jordi and Espada with: 2-0"
+sh addGame.sh Jordi Espada 2 0
+
+echo "Adding a game between Jordi and Espada with: 1-2"
+sh addGame.sh Jordi Espada 1 2
+
+echo "Adding a game between Jordi and Seb with: 1-2"
+sh addGame.sh Jordi Seb 1 2
+
+echo "Adding a game between Jordi and Seb with: 3-4"
+sh addGame.sh Jordi Seb 3 4
+
+echo "Adding a game between Jordi and Seb with: 2-2"
+sh addGame.sh Jordi Seb 2 2
+
+echo "Adding a game between Pepe and Valenti with: 4-0"
+sh addGame.sh Pepe Valenti 4 0
+
+echo "Adding a game between Pepe and Jordi with: 0-0"
+sh addGame.sh Pepe Jordi 0 0
+
+echo "Adding a game between Pepe and Jordi with: 4-0"
+sh addGame.sh Pepe Jordi 4 0
+
+echo "Adding a game between Espada and Oriol with: 1-1"
+sh addGame.sh Espada Oriol 1 1
+
+echo "Adding a game between Espada and Oriol with: 0-0"
+sh addGame.sh Espada Oriol 0 0
+
+echo "Adding a game between Espada and Oriol with: 3-1"
+sh addGame.sh Espada Oriol 3 1
+
+echo "Adding a game between Jordi and Valenti with: 2-0"
+sh addGame.sh Jordi Valenti 2 0
+
+echo "Adding a game between Jordi and Rafa with: 3-2"
+sh addGame.sh Jordi Rafa 3 2
+
+echo "Adding a game between Jordi and Rafa with: 3-0"
+sh addGame.sh Jordi Rafa 3 0
+
+echo "Adding a game between Jordi and Pepe with: 2-0"
+sh addGame.sh Jordi Pepe 2 0
+
+echo "Adding a game between Pepe and Rafa with: 2-0"
+sh addGame.sh Pepe Rafa 2 0
+
+echo "Adding a game between Mickey and Pepe with: 1-5"
+sh addGame.sh Mickey Pepe 1 5
+
+echo "Adding a game between Mickey and Pepe with: 1-3"
+sh addGame.sh Mickey Pepe 1 3
+
+echo "Adding a game between Jordi and Oriol with: 0-1"
+sh addGame.sh Jordi Oriol 0 1
+
+echo "Adding a game between Pepe and Oriol with: 2-0"
+sh addGame.sh Pepe Oriol 2 0
+
+echo "Adding a game between Jordi and Rafa with: 2-2"
+sh addGame.sh Jordi Rafa 2 2
+
+echo "Adding a game between Espada and Oriol with: 1-2"
+sh addGame.sh Espada Oriol 1 2
+
+echo "Adding a game between Jordi and Oriol with: 1-1"
+sh addGame.sh Jordi Oriol 1 1
+
+echo "Adding a game between Espada and Oriol with: 1-0"
+sh addGame.sh Espada Oriol 1 0
+
+echo "Adding a game between Espada and Jordi with: 1-0"
+sh addGame.sh Espada Jordi 1 0
+
+echo "Adding a game between Mickey and Jordi with: 1-1"
+sh addGame.sh Mickey Jordi 1 1
+
+echo "Adding a game between Mickey and Jordi with: 1-2"
+sh addGame.sh Mickey Jordi 1 2
+
+echo "Adding a game between Mickey and Jordi with: 3-2"
+sh addGame.sh Mickey Jordi 3 2
+
+echo "Adding a game between Pepe and Mickey with: 0-0"
+sh addGame.sh Pepe Mickey 0 0
+
+echo "Adding a game between Pepe and Mickey with: 2-1"
+sh addGame.sh Pepe Mickey 2 1
+
+echo "Adding a game between Jordi and Valenti with: 3-1"
+sh addGame.sh Jordi Valenti 3 1
+
+echo "Adding a game between Rafa and Valenti with: 3-1"
+sh addGame.sh Rafa Valenti 3 1
+
+echo "Adding a game between Rafa and Oriol with: 1-1"
+sh addGame.sh Rafa Oriol 1 1
+
+echo "Adding a game between Rafa and Joan with: 2-2"
+sh addGame.sh Rafa Joan 2 2
+
+echo "Adding a game between Quique and Oriol with: 3-1"
+sh addGame.sh Quique Oriol 3 1
+
+echo "Adding a game between Quique and Oriol with: 0-0"
+sh addGame.sh Quique Oriol 0 0
+
+echo "Adding a game between Quique and Rafa with: 1-0"
+sh addGame.sh Quique Rafa 1 0
+
+echo "Adding a game between Oriol and Rafa with: 1-3"
+sh addGame.sh Oriol Rafa 1 3
+
+echo "Adding a game between Oriol and Pepe with: 1-0"
+sh addGame.sh Oriol Pepe 1 0
+
+echo "Adding a game between Valenti and Jordi with: 2-1"
+sh addGame.sh Valenti Jordi 2 1
+
+echo "Adding a game between Espada and Quique with: 0-3"
+sh addGame.sh Espada Quique 0 3
+
+echo "Adding a game between Rafa and Seb with: 0-0"
+sh addGame.sh Rafa Seb 0 0
+
+echo "Adding a game between Espada and Seb with: 2-1"
+sh addGame.sh Espada Seb 2 1
+
+echo "Adding a game between Espada and Jordi with: 2-0"
+sh addGame.sh Espada Jordi 2 0
+
+echo "Adding a game between Pepe and Rafa with: 3-0"
+sh addGame.sh Pepe Rafa 3 0
+
+echo "Adding a game between Pepe and Rafa with: 1-2"
+sh addGame.sh Pepe Rafa 1 2
+
+echo "Adding a game between Valenti and Quique with: 0-3"
+sh addGame.sh Valenti Quique 0 3
+
+echo "Adding a game between Oriol and Quique with: 0-2"
+sh addGame.sh Oriol Quique 0 2
+
+echo "Adding a game between Rafa and Pepe with: 2-4"
+sh addGame.sh Rafa Pepe 2 4
+
+echo "Adding a game between Espada and Oriol with: 1-1"
+sh addGame.sh Espada Oriol 1 1
+
+echo "Adding a game between Espada and Oriol with: 1-1"
+sh addGame.sh Espada Oriol 1 1
+
+echo "Adding a game between Espada and Rafa with: 0-2"
+sh addGame.sh Espada Rafa 0 2
+
+echo "Adding a game between Espada and Pepe with: 1-0"
+sh addGame.sh Espada Pepe 1 0
+
+echo "Adding a game between Valenti and Jordi with: 2-2"
+sh addGame.sh Valenti Jordi 2 2
+
+echo "Adding a game between Valenti and Pepe with: 0-0"
+sh addGame.sh Valenti Pepe 0 0
+
+echo "Adding a game between Pepe and Jordi with: 4-0"
+sh addGame.sh Pepe Jordi 4 0
+
+echo "Adding a game between Jordi and Rafa with: 2-2"
+sh addGame.sh Jordi Rafa 2 2
+
+echo "Adding a game between Joan and Rafa with: 5-0"
+sh addGame.sh Joan Rafa 5 0
+
+echo "Adding a game between Espada and Oriol with: 1-0"
+sh addGame.sh Espada Oriol 1 0
+
+echo "Adding a game between Joan and Rafa with: 0-0"
+sh addGame.sh Joan Rafa 0 0
+
+echo "Adding a game between Joan and Pepe with: 2-0"
+sh addGame.sh Joan Pepe 2 0
+
+echo "Adding a game between Espada and Valenti with: 4-1"
+sh addGame.sh Espada Valenti 4 1
+
+echo "Adding a game between Espada and Rafa with: 1-0"
+sh addGame.sh Espada Rafa 1 0
+
+echo "Adding a game between Jordi and Oriol with: 1-6"
+sh addGame.sh Jordi Oriol 1 6
+
+echo "Adding a game between Espada and Jordi with: 1-1"
+sh addGame.sh Espada Jordi 1 1
+
+echo "Adding a game between Espada and Jordi with: 1-1"
+sh addGame.sh Espada Jordi 1 1
+
+echo "Adding a game between Espada and Seb with: 1-2"
+sh addGame.sh Espada Seb 1 2
+
+echo "Adding a game between Seb and Jordi with: 3-1"
+sh addGame.sh Seb Jordi 3 1
+
+echo "Adding a game between Seb and Jordi with: 3-0"
+sh addGame.sh Seb Jordi 3 0
+
+echo "Adding a game between Rafa and Espada with: 2-0"
+sh addGame.sh Rafa Espada 2 0
+
+echo "Adding a game between Rafa and Oriol with: 2-0"
+sh addGame.sh Rafa Oriol 2 0
+
+echo "Adding a game between Espada and Quique with: 0-4"
+sh addGame.sh Espada Quique 0 4
+
+echo "Adding a game between Quique and Pepe with: 1-2"
+sh addGame.sh Quique Pepe 1 2
+
+echo "Adding a game between Espada and Joan with: 1-1"
+sh addGame.sh Espada Joan 1 1
+
+echo "Adding a game between Quique and Joan with: 2-1"
+sh addGame.sh Quique Joan 2 1
+
+echo "Adding a game between Rafa and Espada with: 2-1"
+sh addGame.sh Rafa Espada 2 1
+
+echo "Adding a game between Joan and Espada with: 2-0"
+sh addGame.sh Joan Espada 2 0
+
+echo "Adding a game between Rafa and Oriol with: 0-1"
+sh addGame.sh Rafa Oriol 0 1
+
+echo "Adding a game between Quique and Oriol with: 1-0"
+sh addGame.sh Quique Oriol 1 0
+
+echo "Adding a game between Quique and Pepe with: 0-1"
+sh addGame.sh Quique Pepe 0 1
+
+echo "Adding a game between Rafa and Joan with: 3-3"
+sh addGame.sh Rafa Joan 3 3
+
+echo "Adding a game between Rafa and Joan with: 0-3"
+sh addGame.sh Rafa Joan 0 3
---
+++ scripts/addPlayers-RAW27.sh
@@ -0,0 +1,29 @@
+echo "Adding player Pepe rosanero@gmail.com"
+sh addPlayer.sh Pepe rosanero@gmail.com
+
+echo "Adding player Mickey mickey.tetrov@gmail.com"
+sh addPlayer.sh Mickey mickey.tetrov@gmail.com
+
+echo "Adding player Seb sebastiaan.vankuyk@gmail.com"
+sh addPlayer.sh Seb sebastiaan.vankuyk@gmail.com
+
+echo "Adding player Jordi jcollell@gmail.com"
+sh addPlayer.sh Jordi jcollell@gmail.com
+
+echo "Adding player Rafa rais38@gmail.com"
+sh addPlayer.sh Rafa rais38@gmail.com
+
+echo "Adding player Joan joanromano@gmail.com"
+sh addPlayer.sh Joan joanromano@gmail.com
+
+echo "Adding player Valenti vfreixanet@gmail.com"
+sh addPlayer.sh Valenti vfreixanet@gmail.com
+
+echo "Adding player Espada ijespada@gmail.com"
+sh addPlayer.sh Espada ijespada@gmail.com
+
+echo "Adding player Oriol oriolblanc@gmail.com"
+sh addPlayer.sh Oriol oriolblanc@gmail.com
+
+echo "Adding player Quique nriquealvarez@gmail.com"
+sh addPlayer.sh Quique nriquealvarez@gmail.com
