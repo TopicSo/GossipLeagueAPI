f23e27afaa806072c363ccc463f945eba65e2ccb
Giuseppe Basile <rosanero@gmail.com>
2013-05-03 10:13:47 +0200

    Internal method for Game Controller

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -11,86 +11,100 @@ import play.mvc.Controller;
 
 public class GameController extends Controller {
 
-	public static void list(String player1Id, String player2Id, int page, int recsPerPage){
+	public static void list(String player1Id, String player2Id, int page,
+			int recsPerPage) {
 		Player player1 = null;
 		Player player2 = null;
 		List<Game> games = null;
-		
-		if(player1Id != null){
+
+		if (player1Id != null) {
 			player1 = Player.findById(player1Id);
 		}
-		
-		if(player2Id != null){
+
+		if (player2Id != null) {
 			player2 = Player.findById(player2Id);
 		}
 
-        if(recsPerPage == 0)
-        	recsPerPage = Game.DEFAULT_RECS_PER_PAGE;
+		if (recsPerPage == 0)
+			recsPerPage = Game.DEFAULT_RECS_PER_PAGE;
 
 		games = Game.findGamesBetween(player1, player2, page, recsPerPage);
-		
+
 		render(games);
 	}
-	
-	public static void listRaw(){
-		
+
+	public static void listRaw() {
+
 		List<Game> games = Game.findAll();
-		
+
 		String output = "";
-		for(Game game: games){
-			output = output + "echo \"Adding a game between " + game.getLocal().getUsername() + " and " + game.getVisitor().getUsername() + " with: " + game.getGolsLocal() + "-" + game.getGolsVisitor() + "\"\n";
-			output = output + "sh addGames.sh " + game.getLocal().getUsername() + " " + game.getVisitor().getUsername() + " " + game.getGolsLocal() + " " + game.getGolsVisitor() + "\n\n";
+		for (Game game : games) {
+			output = output + "echo \"Adding a game between "
+					+ game.getLocal().getUsername() + " and "
+					+ game.getVisitor().getUsername() + " with: "
+					+ game.getGolsLocal() + "-" + game.getGolsVisitor()
+					+ "\"\n";
+			output = output + "sh addGames.sh " + game.getLocal().getUsername()
+					+ " " + game.getVisitor().getUsername() + " "
+					+ game.getGolsLocal() + " " + game.getGolsVisitor()
+					+ "\n\n";
 		}
-		
+
 		renderText(output);
 	}
-	
-	
-    public static void addGame(@Required String localPlayer, @Required String visitorPlayer, @Required int localGoals, @Required int visitorGoals) throws Exception{
-    	boolean added = false;
-    	Game game = null;
-    	
-    	if(localPlayer == null || visitorPlayer == null){
-    		render(added, game);
-    	}
-    
-    	Player local = Player.findById(localPlayer);
-    	if(local == null){
-    		local = Player.findByUsername(localPlayer);
-    	}
-    	
-    	Player visitor = Player.findById(visitorPlayer);
-    	if(visitor == null){
-    		visitor = Player.findByUsername(visitorPlayer);
-    	}
-    	if(local == null || visitor == null){
-    		throw new Exception("Unexpected Players");
-    	}
-   
-    	try{    		
-    		game = new Game(local, visitor, localGoals, visitorGoals);
-    		added = true;
-    		game.save();
-    	}
-    	catch(GameInvalidModelException e){
-    		render(added, game);
-    	}
-    
-    	LeagueScoreEngine scoreEngine = new LeagueScoreEngine();
-    	scoreEngine.evaluateFriendshipGame(game);
-    	
-    	game.save();
-    	
-    	// update stats
-    	local.save();
-    	visitor.save();
-    	
-        render(added, game);
-    }
-    
-    public static void deleteAll(){
-    
-    	Game.deleteAll();
-    	render();
-    }
+
+	public static void addGame(@Required String localPlayer,
+			@Required String visitorPlayer, @Required int localGoals,
+			@Required int visitorGoals) throws Exception {
+		boolean added = false;
+		Game game = null;
+
+		if (localPlayer == null || visitorPlayer == null) {
+			render(added, game);
+		}
+
+		Player local = Player.findById(localPlayer);
+		if (local == null) {
+			local = Player.findByUsername(localPlayer);
+		}
+
+		Player visitor = Player.findById(visitorPlayer);
+		if (visitor == null) {
+			visitor = Player.findByUsername(visitorPlayer);
+		}
+		
+		if (local == null || visitor == null) {
+			throw new Exception("Unexpected Players");
+		}
+
+		game = addGameInternal(local, visitor, localGoals, visitorGoals);
+		added = game != null;
+		render(added, game);
+	}
+
+	public static Game addGameInternal(Player local, Player visitor,
+			int localGoals, int visitorGoals) {
+		Game game = null;
+		try {
+			game = new Game(local, visitor, localGoals, visitorGoals);
+			game.save();
+		} catch (GameInvalidModelException e) {
+			return null;
+		}
+
+		LeagueScoreEngine scoreEngine = new LeagueScoreEngine();
+		scoreEngine.evaluateFriendshipGame(game);
+
+		game.save();
+
+		// update stats
+		local.save();
+		visitor.save();
+		return game;
+	}
+
+	public static void deleteAll() {
+		Game.deleteAll();
+		render();
+	}
 }
